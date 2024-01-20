9867addca1635efc9fe689b3df551ffabfff6896
WackyModer <78763021+WackyModer@users.noreply.github.com>
2024-01-20 09:54:25 -0800

    Update plugin.cs

--- ModNames/plugin.cs
+++ ModNames/plugin.cs
@@ -36,31 +36,26 @@ namespace ModNames
         void spawnshit()
         {
 
-            Canvas canvas = current_canvas; //FindObjectOfType<Canvas>();
-            //Plugin.print(canvas.name);
+            Canvas canvas = current_canvas;
+            
             canvas = GameObject.Find("Canvas (1)").GetComponent<Canvas>();
-            //Plugin.print(canvas.name);
+            
             if (canvas == null)
             {
                 Plugin.print("WTF?!??! There was no canvas object!");
                 return;
             }
 
-            //GameObject ver_txt = GameObject.Find("Canvas (1)/TEXT");
-
 
             int plugin_num = 0;
             foreach (var plugin in Chainloader.PluginInfos)
             {
 
-                // Create a new text box using TextMeshProUGUI
                 mod_text = new GameObject("Mod_List_TextBox", typeof(RectTransform), typeof(TextMeshProUGUI));
                 mod_text.transform.SetParent(canvas.transform);
 
-                // Configure TextMeshPro properties
                 TextMeshProUGUI textComponent = mod_text.GetComponent<TextMeshProUGUI>();
-                //textComponent.text = "Super long text so that I can test an edge case that should really *NEVER* be met but I have to because this is not a standardized thing.";
-
+                
                 textComponent.raycastTarget = false;
 
                 if (plugin.Key.Split('.').Length == 3)
@@ -82,11 +77,11 @@ namespace ModNames
                 }
 
                 textComponent.font = LocalizedText.localizationTable.GetFont(Settings.Get().Language, false);
-                textComponent.color = Color.black;//new Color((float)20/255, (float)94/255, (float)146/255);
+                textComponent.color = Color.black;
 
                 textComponent.raycastTarget = false;
 
-                textComponent.fontSize = 0.27f;//20;
+                textComponent.fontSize = 0.27f;
                 textComponent.alignment = TextAlignmentOptions.Right;
 
 
@@ -99,12 +94,11 @@ namespace ModNames
                 rectTransform.anchorMax = new Vector2(1, 0);
                 rectTransform.sizeDelta = new Vector2(7, 0.5f);
 
-                rectTransform.anchoredPosition = new Vector2(-30, 100 + (80 * plugin_num)); // Adjust the values as needed  (-30,100+(90*plugin_num));
+                rectTransform.anchoredPosition = new Vector2(-30, 100 + (80 * plugin_num));
                 rectTransform.pivot = new Vector2(1, 0);
                 plugin_num++;
             }
 
-            // Make the text box visible
             mod_text.SetActive(true);
         }
 
@@ -112,7 +106,6 @@ namespace ModNames
         {
             string currentScene = SceneManager.GetActiveScene().name;
 
-            //Plugin.print(currentScene);
             if (currentScene != "MainMenu")
             {
                 try {
@@ -137,7 +130,6 @@ namespace ModNames
                 }
                 if (!do_i_have_it)
                 {
-                    //Plugin.print("dodjaofjsd");
                     spawnshit();
                 }
             }
@@ -145,24 +137,16 @@ namespace ModNames
 
         void Start()
         {
-
-
-            // OK I GUESS I HAVE TO MAKE MY OWN FUCKING CANVAS CAUSE BOPL IS A SHITHEAD
-            // Create a new GameObject
             GameObject canvasObject = new GameObject("ModNamesCanvas");
 
-            // Add Canvas component to the GameObject
             Canvas canvas = canvasObject.AddComponent<Canvas>();
             canvas.renderMode = RenderMode.WorldSpace;
             current_canvas = canvas;
-            // Add CanvasScaler component for responsive design (optional)
             canvasObject.AddComponent<CanvasScaler>();
 
 
-            // unused, may never be used
             float canv_h = canvas.GetComponent<RectTransform>().rect.height;
 
-            // Make the text box visible
             Plugin.print(canv_h);
 
 
